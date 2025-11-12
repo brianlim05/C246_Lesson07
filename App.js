0b5ca419e42ca401be05d9cea9e7175d844a4e15
brianlim05 <brianlim05@hotmail.com>
2025-11-12 11:21:06 +0800

    Final

--- App.js
+++ App.js
@@ -1,20 +1,178 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Exercise2 = () => {
+    return (
+        <View>
+
+            <View style={styles.greenBox}>
+                <Text style={[styles.title, styles.boxText]}>Who We Are</Text>
+            </View>
+
+
+            <View style={styles.greenBox}>
+                <Text style={styles.boxText}>Our People</Text>
+            </View>
+
+
+            <View style={styles.greenBox}>
+                <Text style={styles.boxText}>Our Campus</Text>
+            </View>
+        </View>
+    );
+};
+
+const Exercise3A = () => {
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, styles.boxText, { backgroundColor: "powderblue"}]}>
+                <Text>Child One</Text>
+            </View>
+
+            <View style={[styles.child, styles.boxText, { backgroundColor: "skyblue" }]}>
+                <Text>Child Two</Text>
+            </View>
+
+            <View style={[styles.child, styles.boxText, { backgroundColor: "steelblue" }]}>
+                <Text>Child Three</Text>
+            </View>
+        </View>
+    );
+};
+
+
+const Exercise3B = () => {
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { flex:4, backgroundColor: "powderblue" }]}>
+                <Text  style={styles.boxText}>Child One</Text>
+            </View>
+
+            <View style={[styles.child, { flex:4, backgroundColor: "skyblue" }]}>
+                <Text style={styles.boxText}>Child Two</Text>
+            </View>
+
+            <View style={[styles.child, {flex:4,backgroundColor: "steelblue"}]}>
+                <Text style={styles.boxText}>Child Three</Text>
+            </View>
+        </View>
+    );
+};
+
+const Exercise3C = () => {
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { flex:4, backgroundColor: "powderblue",maxWidth:"90"  }]}>
+                <Text  style={styles.boxText}>Child One</Text>
+            </View>
+
+            <View style={[styles.child, { flex:4, backgroundColor: "skyblue"}]}>
+                <Text style={styles.boxText}>Child Two</Text>
+            </View>
+
+            <View style={[styles.child, {flex:4,backgroundColor: "steelblue",maxHeight:"120"}]}>
+                <Text style={styles.boxText}>Child Three</Text>
+            </View>
+        </View>
+    );
+};
+
+const Exercise3D = () => {
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { backgroundColor: "powderblue", flex: 1 }]}>
+                <Text style={styles.text}>Child One</Text>
+            </View>
+
+            <View style={[styles.child, { backgroundColor: "skyblue", flex: 2 }]}>
+                <Text style={styles.text}>Child Two</Text>
+            </View>
+
+            <View style={[styles.child, { backgroundColor: "steelblue", flex: 3 }]}>
+                <Text style={styles.text}>Child Three</Text>
+            </View>
+        </View>
+    );
+};
+
+const Exercise3E = () => {
+    return (
+        <View style={styles.parent}>
+            <View style={[styles.child, { backgroundColor: "lightblue",justifyContent: "center",alignItems:"center" }]}>
+                <Text>Square 1</Text>
+            </View>
+
+            <View style={[styles.child, { backgroundColor: "lightgreen",justifyContent: "center",alignItems:"center" }]}>
+                <Text >Square 2</Text>
+            </View>
+
+            <View style={[styles.child, { backgroundColor: "red", justifyContent: "center",alignItems:"center" }]}>
+                <Text>Square 3</Text>
+            </View>
+        </View>
+    );
+};
+
+const Exercise4 = () => {
+    return (
+        <View style={styles.parent}></View>
+    )
 }
 
+export default Exercise3E;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    greenBox: {
+        width: 100,
+        height: 100,
+        marginTop: 30,
+        backgroundColor: "green",
+        borderWidth: 1,
+        borderColor: "black",
+    },
+
+    boxText: {
+        textAlign: "center",
+        color: "white",
+    },
+
+    title: {
+        fontWeight: "bold",
+    },
+
+    // parent: {
+    //     flexDirection:"column",
+    //     backgroundColor: '#F5fcff',
+    //     borderColor: '#0099AA',
+    //     borderWidth:5,
+    //     // justifyContent: "flex-start",
+    //     // justifyContent: "flex-end",
+    //     // justifyContent: "center",
+    //     // justifyContent: "space-between",
+    //     // justifyContent: "space-around",
+    //     // justifyContent: "space-evenly",
+    //     flex:1,
+    //
+    // },
+    //
+    // Child: {
+    //     borderWidth: 2,
+    //     textAlign: "center",
+    //     fontSize: 24,
+    //     flex:1,
+    // }
+    parent: {
+        backgroundColor: "whiteSmoke",
+        marginTop:30,
+        flex:1,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+
+    child:  {
+        width:80,
+        height:80,
+    }
+
 });
